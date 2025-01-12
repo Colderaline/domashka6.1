8940000e4c5e7d7cb0b4bf9cfd569b1f1c53ae6a
Colderaline <coldera.line@gmail.com>
2025-01-12 15:24:31 +0200

    Initial commit

---
+++ src/app/Main.java
@@ -0,0 +1,37 @@
+package app;
+
+public class Main {
+
+    public static void main(String[] args){
+
+        int sum = 0;
+
+
+        for (int number = 1; number <= 6; number++) {
+            sum += number;
+            System.out.println(number + ") Num is " + number + ", sum is " + sum);
+        }
+
+        System.out.println("------------------------");
+        System.out.println("Sum of numbers is " + sum);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
